7ce3321110c7d18ad5eeb47e5abf5f8363f7d7c9
yeongjong310 <tohero21@naver.com>
2022-01-14 02:17:31 +0900

    feat: slide 목업 데이터 추가

---
+++ wanted/src/Component/Carousel/slideImgs.ts
@@ -0,0 +1,48 @@
+const slideImgs = [
+  {
+    id: 0,
+    src: 'https://static.wanted.co.kr/images/banners/1436/e2dd9445.thumb_1006_280.jpg',
+  },
+  {
+    id: 1,
+    src: 'https://static.wanted.co.kr/images/banners/1452/be4ec643.thumb_1006_280.jpg',
+  },
+  {
+    id: 2,
+    src: 'https://static.wanted.co.kr/images/banners/1460/619f3af7.thumb_1006_280.jpg',
+  },
+  {
+    id: 3,
+    src: 'https://static.wanted.co.kr/images/banners/1435/6cdcea85.thumb_1006_280.jpg',
+  },
+  {
+    id: 4,
+    src: 'https://static.wanted.co.kr/images/banners/1438/015566ac.thumb_1006_280.jpg',
+  },
+  {
+    id: 5,
+    src: 'https://static.wanted.co.kr/images/banners/1453/7a978579.thumb_1006_280.jpg',
+  },
+  {
+    id: 6,
+    src: 'https://static.wanted.co.kr/images/banners/1468/3df61cbc.thumb_1006_280.jpg',
+  },
+  {
+    id: 7,
+    src: 'https://static.wanted.co.kr/images/banners/1434/fdbbcb06.thumb_1006_280.jpg',
+  },
+  {
+    id: 8,
+    src: 'https://static.wanted.co.kr/images/banners/1451/725c6862.thumb_1006_280.jpg',
+  },
+  {
+    id: 9,
+    src: 'https://static.wanted.co.kr/images/banners/1473/41f7b36e.thumb_1006_280.jpg',
+  },
+  {
+    id: 10,
+    src: 'https://static.wanted.co.kr/images/banners/1454/e504b006.thumb_1006_280.jpg',
+  },
+];
+
+export default slideImgs;
