9bb29ce7aad7db898a4c5b2c530a569f73d82635
yeongjong310 <tohero21@naver.com>
2022-01-14 17:05:44 +0900

    feat: 슬라이드 디자인(데스크탑 위치) 및 무한 슬라이드 기능 구현

---
+++ wanted/src/Component/Carousel/Carousel.styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledCarousel = styled.div``;
+
+export const StyledCarouselSlider = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  li {
+    padding: 0 12px;
+  }
+`;
--- wanted/src/Component/Carousel/Carousel.tsx
+++ wanted/src/Component/Carousel/Carousel.tsx
@@ -1,16 +1,76 @@
-import React, { ReactElement } from 'react';
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable operator-linebreak */
+// eslint-disable-next-line object-curly-newline
+import React, { ReactElement, useRef, useState } from 'react';
+import { StyledCarousel, StyledCarouselSlider } from './Carousel.styled';
 import slideImgs from './slideImgs';
 
 export default function Carousel(): ReactElement {
+  const carouselRef = useRef<HTMLUListElement>(null);
+  const [currentSlide, setCurrentSlide] = useState<number>(2);
+  const imageWidth = carouselRef.current?.children[0].clientWidth || 1015;
+  let isMoving = false;
+
+  const moveSlide = (curSlide: number): void => {
+    if (!carouselRef.current || isMoving) return;
+    isMoving = true;
+    carouselRef.current.style.transition = 'transform 500ms ease-in-out';
+    carouselRef.current.style.transform = `translate3D(${
+      curSlide * -imageWidth + (window.innerWidth - imageWidth) / 2
+    }px, 0, 0)`;
+
+    setTimeout(() => {
+      if (!carouselRef.current) return;
+
+      carouselRef.current.style.transition = 'none';
+      setCurrentSlide(
+        curSlide === carouselRef.current.children.length - 2
+          ? 2
+          : curSlide === 1
+          ? carouselRef.current.children.length - 3
+          : curSlide,
+      );
+    }, 500);
+  };
+
   return (
-    <div>
-      <ul>
-        <li>
-          {slideImgs.map(({ src }) => (
-            <img src={src} alt="" />
-          ))}
-        </li>
-      </ul>
-    </div>
+    <StyledCarousel>
+      <StyledCarouselSlider
+        ref={carouselRef}
+        style={{
+          transform: `translate3D(${
+            currentSlide * -imageWidth + (window.innerWidth - imageWidth) / 2
+          }px, 0, 0)`,
+        }}
+      >
+        {[
+          ...slideImgs.slice(slideImgs.length - 2, slideImgs.length),
+          ...slideImgs,
+          ...slideImgs.slice(0, 2),
+        ].map(({ id, src }, index) => (
+          <li key={id + index}>
+            <img src={src} alt={`slide${id}`} />
+          </li>
+        ))}
+      </StyledCarouselSlider>
+      <button
+        type="button"
+        onClick={() => {
+          moveSlide(currentSlide - 1);
+        }}
+      >
+        prev
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          moveSlide(currentSlide + 1);
+        }}
+      >
+        next
+      </button>
+    </StyledCarousel>
   );
 }
