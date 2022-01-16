54a959c630ab7c0984001d0b893a21e860528dc4
yeongjong310 <tohero21@naver.com>
2022-01-17 01:18:53 +0900

    feat: Carousel에서 사용할 버튼 디자인 컴포넌트로 구현

---
+++ wanted/src/Component/Carousel/CarouselArrowButton.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import ArrowButton from '../ArrowButton/ArrowButton';
+
+const CarouselArrowButton = styled(ArrowButton)`
+  width: 30px;
+  height: 60px;
+  opacity: 0.5;
+  border-radius: 15px;
+`;
+export default CarouselArrowButton;
