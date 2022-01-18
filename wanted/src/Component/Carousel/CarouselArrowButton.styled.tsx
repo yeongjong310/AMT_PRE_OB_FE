7d0b0e297557a679c2ccf02ea597d2db96cb037d
yeongjong310 <tohero21@naver.com>
2022-01-18 16:57:28 +0900

    feat: disable 한 eslint 설정 코드 삭제

--- wanted/src/Component/Carousel/CarouselArrowButton.styled.tsx
+++ wanted/src/Component/Carousel/CarouselArrowButton.styled.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-confusing-arrow */
-/* eslint-disable implicit-arrow-linebreak */
 import styled from 'styled-components/macro';
 import ArrowButton from '../ArrowButton/ArrowButton';
 
