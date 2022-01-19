38a7d5f7d302b2161290a1376c13361871428197
yeongjong310 <tohero21@naver.com>
2022-01-19 18:13:55 +0900

    feat: Nav App에 연동

--- wanted/src/Component/App/App.tsx
+++ wanted/src/Component/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
 import slideImgs from '../../data/slideImgs';
+import Nav from '../Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Carousel imgs={slideImgs} duration={500} />
     </div>
   );
